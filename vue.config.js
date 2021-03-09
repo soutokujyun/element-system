b2b0244be44153bdabde7ff59333038dc19e6d40
zengdexun <vp00792@globalymc.com>
2021-03-09 09:02:07 +0800

    2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports={
     devServer:{
         open: true
+    },
+    configureWebpack: {
+        resolve :{
+            alias: {
+                'src': path.resolve(__dirname, '../src'),
+                'components': path.resolve(__dirname, '../src/components'),
+                'api': path.resolve(__dirname, '../src/api'),
+                'utils': path.resolve(__dirname, '../src/utils'),
+                'store': path.resolve(__dirname, '../src/store'),
+                'router': path.resolve(__dirname, '../src/router')
+            }
+        }
     }
 }
